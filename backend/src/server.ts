a74885d2d24b947d76407404fba58c0d9e2ed7c2
shreyanshkhatri <shreyanshkhatri8@gmail.com>
2024-12-31 09:48:15 +0530

    testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,15 @@ const morgan = require('morgan');
 dotenv.config();
 connectDB();
 app.use(morgan('tiny'));
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [process.env.FRONTEND_URL];
+console.log(process.env.FRONTEND_URL);
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // Allow cookies/auth headers if needed
+  }),
+);
 
 app.use(express.json());
 app.use('/api', excelRoutes);
